b6da63a9538b40e110ef077a97c2451b400dd16b
nithiishkumarb <nithiishkumarb@gmail.com>
2023-05-31 18:18:18 +0530

    Profile ongoing

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,17 +1,31 @@
 import "./profile.css"
-import Sidebar from "../../components/sidebar/sidebar.jsx"
+import Sidebar from "../../components/sidebar/Sidebar.jsx"
 import Topbar from "../../components/topbar/Topbar"
 import Rightbar from "../../components/rightbar/Rightbar"
-export default function Profile() {
+import Feed from "../../components/feed/Feed"
+export default function Profile(){
   return(
     <>
-    <Topbar/>
-    <div className="profile">
+      <Topbar/>
+      <div className="profile">
         <Sidebar/>
-        <Rightbar/>
-    </div>
-    </>
+        <div className="profileright">
+          <div className="profilerighttop">
+            <div className="profilecover">
+            <img src="../../assests/gift.jpg" alt="Profilecoverimg" className="profilecoverimg" />
+            <img src="../../assests/person/profile1.jpg" alt="profilepicture" className="profilepicture" />
+            </div>
+            <div className="profileinfo">
+              <h4 className="profilename">Nithish</h4>
+              <span className="profiledesc">hi my friends</span>
+            </div>
+          </div>
+          <div className="profilerightbottom">
+            <Feed/>
+            <Rightbar/>
+          </div>
+        </div>
+      </div>
+   </>
   )
 }
-
-
